5cdee735923c556c6bf324aab3239d17423a16f6
Manoj Naidu <manojnaidu619@gmail.com>
2020-03-17 17:25:42 +0530

    Handling error

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component{
-   render(){
-       window.navigator.geolocation.getCurrentPosition(
-           (position) => console.log(position),
-           (error) => console.log(error)
-       )
+    constructor(props){
+        super(props)
+        this.state = {lat: 'loading...', errorMessage: ''}
 
-       return <div>Latitide : </div>
-   }
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                console.log(position)
+                this.setState({lat: position.coords.latitude})
+            },
+            (error) => this.setState({errorMessage: error.message})
+        )
+    }
+
+    render(){
+        return (<div>
+            <h3>{this.state.errorMessage}</h3>
+            Latitude : {this.state.lat}
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
