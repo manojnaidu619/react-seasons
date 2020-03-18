a2817cfec0715a883b94da81cf0f4ddab2f27494
Manoj Naidu <manojnaidu619@gmail.com>
2020-03-18 09:14:01 +0530

    Added lifecycle methods

--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ import ReactDOM from 'react-dom'
 class App extends React.Component{
     constructor(props){
         super(props)
+        console.log("From constructor")
         this.state = {lat: 'loading...', errorMessage: ''}
+    }
 
+    componentDidMount(){
+        console.log("From component did mount")
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
-                console.log(position)
                 this.setState({lat: position.coords.latitude})
             },
             (error) => this.setState({errorMessage: error.message})
         )
     }
 
+    componentDidUpdate(){
+        console.log("From component did update")
+    }
+
     render(){
+        console.log("From render")
         return (<div>
             <h3>{this.state.errorMessage}</h3>
             Latitude : {this.state.lat}
