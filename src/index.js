f76531ba5e420c6dd75d23ccbfd5fc3320fd50dd
Manoj Naidu <manojnaidu619@gmail.com>
2020-03-18 09:20:11 +0530

    Removed constructor function

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component{
-    constructor(props){
-        super(props)
-        console.log("From constructor")
-        this.state = {lat: 'loading...', errorMessage: ''}
-    }
+    state = {lat: 'loading...', errorMessage: ''}
 
     componentDidMount(){
-        console.log("From component did mount")
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({lat: position.coords.latitude})
@@ -18,12 +13,7 @@ class App extends React.Component{
         )
     }
 
-    componentDidUpdate(){
-        console.log("From component did update")
-    }
-
     render(){
-        console.log("From render")
         return (<div>
             <h3>{this.state.errorMessage}</h3>
             Latitude : {this.state.lat}
