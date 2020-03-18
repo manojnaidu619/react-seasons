e0b6a42f84d696a0e3bc560b51efcd1ffe323f5d
Manoj Naidu <manojnaidu619@gmail.com>
2020-03-18 10:48:48 +0530

    Added SeasonConfig for easy tweaking of data

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,18 +1,34 @@
 import React from 'react'
 
+const SeasonConfig = {
+    summer: {
+        text: "Let's hit the Beach!",
+        iconName: "icon sun"
+    },
+    winter: {
+        text: "Burr.. It's cold",
+        iconName: "icon snowflake"
+    }
+}
+
 const getSeason = (lat, month) => {
     if(month > 2 && month < 9){
-        return lat > 0 ? "Summer" : "Winter"
+        return lat > 0 ? "summer" : "winter"
     }else{
-        return lat < 0 ? "Winter" : "Summer"
+        return lat < 0 ? "winter" : "summer"
     }
 }
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth())
+    const {text, iconName} = SeasonConfig[season]
 
     return(
-        <div>current Latitude = {season} </div>
+        <div>
+            <i className={iconName}></i>
+            <h1>{text}</h1>
+            <i className={iconName}></i>
+        </div>
     )
 }
 
